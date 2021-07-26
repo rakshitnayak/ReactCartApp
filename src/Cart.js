c60526c4fb551c34f742cf13057a46b7467858ca
rakshitnayak <rdsonnn@gmail.com>
2021-07-26 19:09:42 +0530

    inital commit

---
+++ src/Cart.js
@@ -0,0 +1,23 @@
+import React from "react";
+import CartItem from "./CartItem";
+
+const cart = (props) => {
+  const { products } = props;
+  return (
+    <div className="cart">
+      {products.map((product) => {
+        return (
+          <CartItem
+            product={product}
+            key={product.id}
+            onIncreaseQuantity={props.onIncreaseQuantity}
+            onDecreaseQuantity={props.onDecreaseQuantity}
+            onDeleteProduct={props.onDeleteProduct}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default cart;
